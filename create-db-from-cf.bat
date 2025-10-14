dd6399ae2a5d9690571fdcb50d6e625db7398135
leontev.i <igvlleont@yandex.ru>
2025-10-14 18:38:03 +0300

    Исправление ошибки
    - после загрузки конфигурации, не обновлялась конфигурация БД

--- create-db-from-cf.bat
+++ create-db-from-cf.bat
@@ -2,4 +2,6 @@
 
 :: set /p dir_1=.cf file path:
 
-:: "C:\Program Files\1cv8\8.3.27.1644\bin\ibcmd.exe" infobase create --config="ПутьКПапкеЭкзепляраСервера\sas-conf.yml" --create-database --load=%dir_1% --apply
+:: "C:\Program Files\1cv8\8.3.27.1644\bin\ibcmd.exe" infobase create --config="ПутьКПапкеЭкзепляраСервера\sas-conf.yml" --create-database --load=%dir_1%
+
+:: "C:\Program Files\1cv8\8.3.27.1644\bin\ibcmd.exe" infobase config apply --config="ПутьКПапкеЭкзепляраСервера\sas-conf.yml"
