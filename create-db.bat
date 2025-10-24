59e815538189f77184e79f097fa069a77aee9232
PHARMC\leontev.i <il@bloft.ru>
2025-10-24 12:39:56 +0300

    Исправление ошибки
    - при создании пустой базы ввод в консоль не нужен

--- create-db.bat
+++ create-db.bat
@@ -1,5 +1,2 @@
-:: @echo off  
-
-:: set /p dir_1=.cf file path:
 
 :: "C:\Program Files\1cv8\8.3.27.1644\bin\ibcmd.exe" infobase create --config="ПутьКПапкеЭкзепляраСервера\sas-conf.yml" --create-database
